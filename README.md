1d013b015e1a638e7cbeed612cefb7709a4c6bf1
Bertrand Bougon <bertrandbougon@gmail.com>
2023-04-09 12:51:22 +0200

    Prepare first release

--- README.md
+++ README.md
@@ -1,27 +1,31 @@
-# Gitlab stats
+# Git stats
 
-Provide statistics usage of your gitlab/github projects
+Provide MR/PR statistics for your Gitlab/GitHub projects
 
 ## Install
+
 Run `npm install -g gitflows-stats`
 
 ## Usage
+
 - `gitflows-stats --help` will print:
+
   ```shell
   Usage: index [options] [command]
-  
+
   Options:
   -h, --help                                        display help for command
-  
+
   Commands:
-  gitlab [options] <token> <projectId> <period>     Provide merge requests statistics on a gitlab project for a given period
-  github [options] <token> <owner> <repo> <period>  Provide pull requests statistics on a github project for a given period
+  gitlab [options] <token> <projectId> <period>     Provide merge requests statistics on a Gitlab project for a given period
+  github [options] <token> <owner> <repo> <period>  Provide pull requests statistics on a GitHub project for a given period
   help [command]                                    display help for command
   ```
+
 - `gitflows-stats gitlab <TOKEN> <PROJECT_ID> <PERIOD>` (<PERIOD> in the following format `2023-01-01,2023-01-31`) will print:
   ```json
   {
-  "average": {
+    "average": {
       "months": 0,
       "days": 4,
       "hours": 22,
@@ -62,13 +66,14 @@ Run `npm install -g gitflows-stats`
   }
   ```
 - `gitflows-stats gitlab <TOKEN> <PROJECT_ID> <PERIOD> --format html` (<PERIOD> in the following format `2023-01-01,2023-01-31`) will generate a `report` folder with an `index.html` where the command has been executed.
-  
+
   **The report will automatically open after generation.**
 
   **example:**
   ![](documentation/chart_screenshot.png)
 
 ### Available output format
+
 - **Console (Default):** print a lightweight statistics in JSON into the console
 - **HTML:** generates a `HTML` file, opening automatically in your browser
 - **CSV:** generates a `CSV` file with all raws data
--- index.ts
+++ index.ts
@@ -1,84 +1,113 @@
 #!/usr/bin/env node
-import {Command, program} from "commander";
-import {MergedRequestHTTPGitlabRepository} from "./src/infrastructure/repository/MergeRequestHTTPGitlabRepository.js";
-import {parseISO} from "date-fns";
-import {ConsoleWriter} from "./src/infrastructure/writer/ConsoleWriter.js";
-import {HTMLWriter} from "./src/infrastructure/writer/HTMLWriter.js";
-import {PullRequestHTTPGithubRepository} from "./src/infrastructure/repository/PullRequestHTTPGithubRepository.js";
-import {CSVWriter} from "./src/infrastructure/writer/CSVWriter.js";
-import {gitStatistics, StatisticsAggregate} from "./src/statistics/GitStatistics.js";
-import {MergeEventRepository} from "./src/statistics/merge-events/MergeEvent.js";
-import {MergeRequestsStatsParameters} from "./src/statistics/Gitlab.js";
-import {PullRequestsStatsParameter} from "./src/statistics/Github.js";
+import { Command, program } from "commander";
+import { MergedRequestHTTPGitlabRepository } from "./src/infrastructure/repository/MergeRequestHTTPGitlabRepository.js";
+import { parseISO } from "date-fns";
+import { ConsoleWriter } from "./src/infrastructure/writer/ConsoleWriter.js";
+import { HTMLWriter } from "./src/infrastructure/writer/HTMLWriter.js";
+import { PullRequestHTTPGithubRepository } from "./src/infrastructure/repository/PullRequestHTTPGithubRepository.js";
+import { CSVWriter } from "./src/infrastructure/writer/CSVWriter.js";
+import { gitStatistics, StatisticsAggregate } from "./src/statistics/GitStatistics.js";
+import { MergeEventRepository } from "./src/statistics/merge-events/MergeEvent.js";
+import { MergeRequestsStatsParameters } from "./src/statistics/Gitlab.js";
+import { PullRequestsStatsParameter } from "./src/statistics/Github.js";
 
-const commaSeparatedList =(list: string) => {
-    return list.split(",")
-}
+const commaSeparatedList = (list: string) => {
+  return list.split(",");
+};
 
 const writer = (format: string): Writer => {
-    if (format === 'html'){
-        return new HTMLWriter("./")
-    }
-    if(format === 'csv') {
-        return new CSVWriter("./")
-    }
-    return new ConsoleWriter()
-}
+  if (format === "html") {
+    return new HTMLWriter("./");
+  }
+  if (format === "csv") {
+    return new CSVWriter("./");
+  }
+  return new ConsoleWriter();
+};
 
 export interface Writer {
-    write(stats: StatisticsAggregate): void
+  write(stats: StatisticsAggregate): void;
 }
 export type RequestParameters = {
-    fromDate: Date;
-    toDate: Date;
-}
+  fromDate: Date;
+  toDate: Date;
+};
 type CommandParameters = {
-    requestParameters: RequestParameters,
-    options: any,
-    token: string
-}
+  requestParameters: RequestParameters;
+  options: any;
+  token: string;
+};
 
-const gitlabCommand = program.command('gitlab')
-    .description('Provide merge requests statistics on a gitlab project for a given period')
-    .argument('<token>', 'your gitlab API token')
-    .argument('<projectId>', 'gitlab project id for which you want to have statistics')
-    .argument('<period>', 'the period you want to analyse (ISO formatted date separated by comma, e.g: 2021-11-02,2021-11-03)', commaSeparatedList);
+const gitlabCommand = program
+  .command("gitlab")
+  .description("Provide merge requests statistics on a Gitlab project for a given period")
+  .argument("<token>", "your gitlab API token")
+  .argument("<projectId>", "gitlab project id for which you want to have statistics")
+  .argument(
+    "<period>",
+    "the period you want to analyse (ISO formatted date separated by comma, e.g: 2021-11-02,2021-11-03)",
+    commaSeparatedList
+  );
 
-const githubCommand = program.command('github')
-    .description('Provide pull requests statistics on a github project for a given period')
-    .argument('<token>', 'your github API token')
-    .argument('<owner>', 'The account owner of the repository. The name is not case sensitive')
-    .argument('<repo>', 'The name of the repository. The name is not case sensitive.')
-    .argument('<period>', 'the period you want to analyse (ISO formatted date separated by comma, e.g: 2021-11-02,2021-11-03)', commaSeparatedList);
+const githubCommand = program
+  .command("github")
+  .description("Provide pull requests statistics on a GitHub project for a given period")
+  .argument("<token>", "your github API token")
+  .argument("<owner>", "The account owner of the repository. The name is not case sensitive")
+  .argument("<repo>", "The name of the repository. The name is not case sensitive.")
+  .argument(
+    "<period>",
+    "the period you want to analyse (ISO formatted date separated by comma, e.g: 2021-11-02,2021-11-03)",
+    commaSeparatedList
+  );
 
+const proceedCommand = (
+  command: Command,
+  commandParameters: (...args: any[]) => CommandParameters,
+  repository: (token: string) => MergeEventRepository
+) => {
+  command
+    .option(
+      "-f, --format <writer>",
+      "format to display the stats (default json in console), available formats are html, csv and console (default)",
+      writer,
+      new ConsoleWriter()
+    )
+    .action((...args: any[]) => {
+      const parameters = commandParameters(...args);
+      gitStatistics(parameters.requestParameters, repository(parameters.token)).then((stats) => {
+        parameters.options.format.write(stats);
+      });
+    });
+};
 
-const proceedCommand = (command: Command, commandParameters: (...args: any[]) => CommandParameters, repository: (token: string) => MergeEventRepository) => {
-    command
-        .option('-f, --format <writer>', 'format to display the stats (default json in console), available formats are html, csv and console (default)', writer, new ConsoleWriter())
-        .action((...args: any[]) => {
-            const parameters = commandParameters(...args)
-            gitStatistics(parameters.requestParameters, repository(parameters.token))
-                .then((stats) => {
-                    parameters.options.format.write(stats)
-                })
-        });
-}
-
-proceedCommand(gitlabCommand, (token: string, projectId: number, period: string[], options) => ({
+proceedCommand(
+  gitlabCommand,
+  (token: string, projectId: number, period: string[], options) => ({
     requestParameters: {
-        fromDate: parseISO(period[0]),
-        projectId: projectId,
-        toDate: parseISO(period[1])
-    } as MergeRequestsStatsParameters, options, token
-}), (token: string) => new MergedRequestHTTPGitlabRepository(token));
+      fromDate: parseISO(period[0]),
+      projectId: projectId,
+      toDate: parseISO(period[1]),
+    } as MergeRequestsStatsParameters,
+    options,
+    token,
+  }),
+  (token: string) => new MergedRequestHTTPGitlabRepository(token)
+);
 
-proceedCommand(githubCommand, (token: string, owner: string, repo: string, period: string[], options) => ({
+proceedCommand(
+  githubCommand,
+  (token: string, owner: string, repo: string, period: string[], options) => ({
     requestParameters: {
-        repo,
-        owner,
-        fromDate: parseISO(period[0]),
-        toDate: parseISO(period[1])
-    } as PullRequestsStatsParameter, options, token
-}), (token: string) => new PullRequestHTTPGithubRepository(token));
+      repo,
+      owner,
+      fromDate: parseISO(period[0]),
+      toDate: parseISO(period[1]),
+    } as PullRequestsStatsParameter,
+    options,
+    token,
+  }),
+  (token: string) => new PullRequestHTTPGithubRepository(token)
+);
 
-program.parse()
+program.parse();
